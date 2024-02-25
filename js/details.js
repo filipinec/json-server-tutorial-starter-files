7c4c2a7dfe1deb25299571af91270e35a9c86f8d
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-02-25 21:04:37 +0100

    get

--- js/details.js
+++ js/details.js
@@ -1,1 +1,25 @@
 // javascript for details.html
+const id = new URLSearchParams(window.location.search).get('id')
+console.log(id);
+const container = document.querySelector(".details")
+
+const renderDetails = async () => {
+  let url = ('http://localhost:3000/posts/' + id)
+  
+  axios.get(url).then(res => {
+  const post = res.data
+  console.log(post)
+  
+  const template = `
+  <h1>${post.title}</h2>
+  <p>${post.body}</p>
+  `
+  container.innerHTML = template
+})
+
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', (e) => renderDetails());
--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 // javascript for index.html
+const container = document.querySelector(".blogs")
 const renderPosts = async () => {
   let url = 'http://localhost:3000/posts'
+  
+  axios.get(url).then(res => {
+  const posts = res.data
+  console.log(posts)
+  let template = '';
+  posts.forEach(post => {
+    template +=  `
+    <div class="post">
+    <h2>${post.title}</h2>
+    <p><small>${post.likes} likes</small></p>
+    <p>${post.body.slice(0,200)}</p>
+    <a href="/details.html?id=${post.id}">read more...</a>
+    </div>
+    `
+  })
+  container.innerHTML = template
+})
+
+
 }
+
 window.addEventListener('DOMContentLoaded', (e) => renderPosts());
